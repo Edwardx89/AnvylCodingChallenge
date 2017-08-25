473e462d4358bbcd0593bb70006f10486ad58387
Edward Goo <Edwardx89@yahoo.com>
2017-08-25 13:23:50 -0400

    boiler plate

---
+++ app/browser/table.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import {Table, Column, Cell} from 'fixed-data-table';
+
+export class Table extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <Table
+        rowsCount={100}
+        rowHeight={50}
+        width={1000}
+        height={500}>
+        <Column
+          cell={<Cell>Basic content</Cell>}
+          width={200}
+        />
+      </Table>
+    )
+  }
+}
